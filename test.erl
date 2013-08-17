d84b21efcaf30ac08f9d8e7e45224c50b3b4f8ab
Jeff Wong <awole20@gmail.com>
2013-08-16 17:11:09 -0700

    adding orddict

--- test.erl
+++ test.erl
@@ -4,11 +4,22 @@
 
 test() ->
     io:format("testing proplist"),
-    proplist(),
-    ok.
+    Proplist = proplist(),
+    Orddict = orddicttest(),
+    {ok, {prop, Proplist}, {orddict, Orddict}}.
 
 proplist() ->
     ProplistInit = [{key1, value1}, {key2, value2}],
     Val = proplists:get_value(key2, ProplistInit),
     io:format("value: ~s\n", [Val]),
     {ok, Val}.
+
+orddicttest() ->
+    Dic = orddict:new(),
+    Dic2 = orddict:store(key1, value1, Dic),
+    Dic3 = orddict:store(key2, value2, Dic2),
+    {ok,Val} = orddict:find(key2, Dic3),
+    error = orddict:find(unknown, Dic3),
+    io:format("value: ~s~n", [Val]),
+    Size = orddict:size(Dic3),
+    {ok, Val, Size}.
