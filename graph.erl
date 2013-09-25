7feb3ee30f58e4e3f916cafe1b8e12a6ed64f9e3
Jeff Wong <awole20@gmail.com>
2013-09-25 16:09:29 -0700

    playing with digraphs

--- graph.erl
+++ graph.erl
@@ -7,9 +7,14 @@ test() ->
     V1 = digraph:add_vertex(G, v1),
     V2 = digraph:add_vertex(G, v2),
     V3 = digraph:add_vertex(G, v3),
+    V4 = digraph:add_vertex(G, v4),
+    _V5 = digraph:add_vertex(G, v5),
     _E1 = digraph:add_edge(G, V1, V2),
     _E2 = digraph:add_edge(G, V2, V3),
-    Path = digraph:get_path(G, V1, V3),
+    _E3 = digraph:add_edge(G, V2, V4),
+    Path = digraph:get_path(G, V1, V4),
+    Sorted = digraph_utils:topsort(G),
     io:format("path: ~p\n", [Path]),
+    io:format("sorted: ~p\n", [Sorted]),
     {ok, {digraph, Path}}.
 
