87a943e46162b2a388eb0644f9500742083a3df0
Jeff Wong <awole20@gmail.com>
2013-09-25 15:52:14 -0700

    adding digraph tests

---
+++ graph.erl
@@ -0,0 +1,15 @@
+-module(graph).
+
+-export([test/0]).
+
+test() ->
+    G = digraph:new(),
+    V1 = digraph:add_vertex(G, v1),
+    V2 = digraph:add_vertex(G, v2),
+    V3 = digraph:add_vertex(G, v3),
+    _E1 = digraph:add_edge(G, V1, V2),
+    _E2 = digraph:add_edge(G, V2, V3),
+    Path = digraph:get_path(G, V1, V3),
+    %% io:format("path: ~s\n", [Path]),
+    {ok, {digraph, Path}}.
+
