2d5c6751215aee8be4ee57065d8e743693e2a4dd
Jeff Wong <awole20@gmail.com>
2013-11-10 13:31:24 -0800

    making genserv actually do

--- genserv/mysrv.erl
+++ genserv/mysrv.erl
@@ -2,19 +2,23 @@
 -behaviour(gen_server).
 
 %% API functions
--export([init/0]).
+-export([start_link/0,say_hello/1,get_response/1]).
 
 %% Gen_server functions
 -export([init/1,handle_call/3,handle_cast/2,handle_info/2,terminate/2,code_change/3]).
 
-init() ->
-    ok.
+start_link() ->
+    gen_server:start_link(?MODULE,[],[]).
+say_hello(Pid) ->
+    gen_server:cast(Pid,"Hello!").
+get_response(Pid) ->
+    gen_server:call(Pid,"Hello!").
 
 init([]) ->
     io:format("init~n"),
     {ok, []}.
 handle_call(Call, From, State) ->
-    io:format("got message ~s from ~s~n",[Call, From]),
+    io:format("got message ~s from ~p~n",[Call, From]),
     {reply, called, State}.
 handle_cast(Msg, State) ->
     io:format("got message ~s~n",[Msg]),
